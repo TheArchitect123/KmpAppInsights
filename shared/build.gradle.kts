aa57408b749d6d31bcadf304b8298f0c4b533632
TheArchitect123 <dan.developer789@gmail.com>
2025-01-25 16:14:38 +1100

    Security Config

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -88,11 +88,14 @@ mavenPublishing {
     // Define credentials for Maven publishing
     repositories {
         maven {
-            name = "snapshots"
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            name = "ossrh"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
                 username = System.getenv("OSSRH_USERNAME") ?: ""
                 password = System.getenv("OSSRH_PASSWORD") ?: ""
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
             }
         }
     }
