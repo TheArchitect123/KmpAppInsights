c859c00a2ec71e69ddaad76ebc97f73d0fc9a913
TheArchitect123 <dan.developer789@gmail.com>
2025-01-25 13:47:06 +1100

    Version 0.5.8

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -81,7 +81,7 @@ afterEvaluate {
         coordinates(
             groupId = "io.github.thearchitect123",
             artifactId = "appInsights",
-            version = "0.5.7"
+            version = "0.5.8"
         )
 
         // Configure POM metadata for the published artifact
