d49217954e724f23fad804c05622c79979bea56f
TheArchitect123 <dan.developer789@gmail.com>
2025-01-25 23:08:51 +1100

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -161,4 +161,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    publishing {
+        singleVariant("release")
+    }
 }
