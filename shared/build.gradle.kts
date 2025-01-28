6cf5a5111cac252252a7216b7568900d1b7eef8d
TheArchitect123 <dan.developer789@gmail.com>
2025-01-28 22:59:34 +1100

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("signing")
     id("maven-publish")
     id("com.vanniktech.maven.publish") version "0.30.0"
-    id("com.gradle.build-scan") version "3.19.1"
+    id("com.gradle.build-scan") version "3.12.1"
 
     kotlin("plugin.serialization") version "2.0.0"
     id("com.google.devtools.ksp")
