63e5ecc870c8170ac5cdbf5bf9c3ee6cf0f9b1cc
TheArchitect123 <dan.developer789@gmail.com>
2025-01-29 00:07:24 +1100

    Test (v1)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -136,3 +136,20 @@ tasks.register("buildAllPlatformsAndPublish") {
         ":shared:publishToMavenCentral"
     )
 }
+
+
+ksp {
+    arg("moduleName", project.name)
+}
+
+android {
+    namespace = "com.architect.kmpappinsights"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
